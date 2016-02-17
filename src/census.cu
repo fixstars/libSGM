26d181263fbf13167f5701b20bbcd7f3ac40458d
yuta.noto <yuta.noto@fixstars.com>
2016-02-17 14:17:06 +0900

    Support 8bit input

--- src/census.cu
+++ src/census.cu
@@ -150,6 +150,136 @@ namespace {
 		}
 	}
 
+
+
+
+
+	template<typename SRC_T>
+	__global__
+		void census_kernel2(int hor, int vert, SRC_T* d_source, uint64_t* d_dest, int width, int height)
+	{
+		const int i = threadIdx.y + blockIdx.y * blockDim.y;
+		const int j = threadIdx.x + blockIdx.x * blockDim.x;
+		const int offset = j + i * width;
+
+		const int rad_h = HOR / 2;
+		const int rad_v = VERT / 2;
+
+		const int swidth = threads_per_block + HOR;
+		const int sheight = threads_per_block + VERT;
+		__shared__ SRC_T s_source[swidth*sheight];
+
+		/**
+		*                  *- blockDim.x
+		*                 /
+		*      +---------+---+ -- swidth (blockDim.x+HOR)
+		*      |         |   |
+		*      |    1    | 2 |
+		*      |         |   |
+		*      +---------+---+ -- blockDim.y
+		*      |    3    | 4 |
+		*      +---------+---+ -- sheight (blockDim.y+VERT)
+		*/
+
+		// 1. left-top side
+		const int ii = threadIdx.y + blockIdx.y * blockDim.y - rad_v;
+		const int jj = threadIdx.x + blockIdx.x * blockDim.x - rad_h;
+		if (ii >= 0 && ii < height && jj >= 0 && jj < width) {
+			s_source[threadIdx.y*swidth + threadIdx.x] = d_source[ii*width + jj];
+		}
+
+		// 2. right side
+		// 2 * blockDim.x >= swidth
+		{
+			const int ii = threadIdx.y + blockIdx.y * blockDim.y - rad_v;
+			const int jj = threadIdx.x + blockIdx.x * blockDim.x - rad_h + blockDim.x;
+			if (threadIdx.x + blockDim.x < swidth && threadIdx.y < sheight) {
+				if (ii >= 0 && ii < height && jj >= 0 && jj < width) {
+					s_source[threadIdx.y*swidth + threadIdx.x + blockDim.x] = d_source[ii*width + jj];
+				}
+			}
+		}
+
+		// 3. bottom side
+		// 2 * blockDim.y >= sheight
+		{
+			const int ii = threadIdx.y + blockIdx.y * blockDim.y - rad_v + blockDim.y;
+			const int jj = threadIdx.x + blockIdx.x * blockDim.x - rad_h;
+			if (threadIdx.x < swidth && threadIdx.y + blockDim.y < sheight) {
+				if (ii >= 0 && ii < height && jj >= 0 && jj < width) {
+					s_source[(threadIdx.y + blockDim.y)*swidth + threadIdx.x] = d_source[ii*width + jj];
+				}
+			}
+		}
+
+		// 4. right-bottom side
+		// 2 * blockDim.x >= swidth && 2 * blockDim.y >= sheight
+		{
+			const int ii = threadIdx.y + blockIdx.y * blockDim.y - rad_v + blockDim.y;
+			const int jj = threadIdx.x + blockIdx.x * blockDim.x - rad_h + blockDim.x;
+			if (threadIdx.x + blockDim.x < swidth && threadIdx.y + blockDim.y < sheight) {
+				if (ii >= 0 && ii < height && jj >= 0 && jj < width) {
+					s_source[(threadIdx.y + blockDim.y)*swidth + threadIdx.x + blockDim.x] = d_source[ii*width + jj];
+				}
+			}
+		}
+		__syncthreads();
+
+		// TODO can we remove this condition?
+		if (rad_v <= i && i < height - rad_v && rad_h <= j && j < width - rad_h)
+		{
+			const int ii = threadIdx.y + rad_v;
+			const int jj = threadIdx.x + rad_h;
+			const int soffset = jj + ii * swidth;
+			// const SRC_T c = d_source[offset];
+			const SRC_T c = s_source[soffset];
+			uint64_t value = 0;
+
+			uint32_t value1 = 0, value2 = 0;
+
+#pragma unroll
+			for (int y = -rad_v; y < 0; y++) {
+				for (int x = -rad_h; x <= rad_h; x++) {
+					// SRC_T result = (c - d_source[width*(i+y)+j+x])>0;
+					SRC_T result = (c - s_source[swidth*(ii + y) + jj + x]) > 0;
+					value1 <<= 1;
+					value1 += result;
+				}
+			}
+
+			int y = 0;
+#pragma unroll
+			for (int x = -rad_h; x < 0; x++) {
+				// SRC_T result = (c - d_source[width*(i+y)+j+x])>0;
+				SRC_T result = (c - s_source[swidth*(ii + y) + jj + x]) > 0;
+				value1 <<= 1;
+				value1 += result;
+			}
+
+#pragma unroll
+			for (int x = 1; x <= rad_h; x++) {
+				// SRC_T result = (c - d_source[width*(i+y)+j+x])>0;
+				SRC_T result = (c - s_source[swidth*(ii + y) + jj + x]) > 0;
+				value2 <<= 1;
+				value2 += result;
+			}
+
+#pragma unroll
+			for (int y = 1; y <= rad_v; y++) {
+				for (int x = -rad_h; x <= rad_h; x++) {
+					// SRC_T result = (c - d_source[width*(i+y)+j+x])>0;
+					SRC_T result = (c - s_source[swidth*(ii + y) + jj + x]) > 0;
+					value2 <<= 1;
+					value2 += result;
+				}
+			}
+
+			value = (uint64_t)value2;
+			value |= (uint64_t)value1 << (rad_v * (2 * rad_h + 1) + rad_h);
+
+			d_dest[offset] = value;
+		}
+	}
 }
 
 
@@ -165,15 +295,22 @@ namespace details {
 				std::cerr << "unsupported census window, only 9x7" << std::endl;
 				return;
 			}
-			if (depth_bits != 16) {
-				std::cerr << "unsupported input bits, only 16 bits" << std::endl;
-				return;
-			}
+			//if (depth_bits != 16) {
+			//	std::cerr << "unsupported input bits, only 16 bits" << std::endl;
+			//	return;
+			//}
 
 			const dim3   blocks((width + threads_per_block - 1) / threads_per_block, (height + threads_per_block - 1) / threads_per_block);
 			const dim3   threads(threads_per_block, threads_per_block);
 
-			census_kernel <<<blocks, threads, 0, cuda_stream >>> (9, 7, (uint16_t*)d_src, d_dst, width, height);
+			//census_kernel <<<blocks, threads, 0, cuda_stream >>> (9, 7, (uint16_t*)d_src, d_dst, width, height);
+
+			if (depth_bits == 16) {
+				census_kernel2<uint16_t> << <blocks, threads, 0, cuda_stream >> > (9, 7, (uint16_t*)d_src, d_dst, width, height);
+			}
+			else if(depth_bits == 8) {
+				census_kernel2<uint8_t> << <blocks, threads, 0, cuda_stream >> > (9, 7, (uint8_t*)d_src, d_dst, width, height);
+			}
 		}
 
 	}
--- src/check_consistency.cu
+++ src/check_consistency.cu
@@ -34,6 +34,22 @@ namespace {
 		}
 	}
 
+	template<typename SRC_T>
+	__global__ void check_consistency_kernel2(uint16_t* d_leftDisp, const uint16_t* d_rightDisp, const SRC_T* d_left, int width, int height)  {
+
+		const int j = blockIdx.x * blockDim.x + threadIdx.x;
+		const int i = blockIdx.y * blockDim.y + threadIdx.y;
+
+		// left-right consistency check, only on leftDisp, but could be done for rightDisp too
+
+		SRC_T mask = d_left[i * width + j];
+		int d = d_leftDisp[i * width + j];
+		int k = j - d;
+		if (mask == 0 || d <= 0 || (k >= 0 && k < width && abs(d_rightDisp[i * width + k] - d) > 1)) {
+			// masked or left-right inconsistent pixel -> invalid
+			d_leftDisp[i * width + j] = 0;
+		}
+	}
 }
 
 namespace sgm {
@@ -44,7 +60,10 @@ namespace sgm {
 			const dim3 blocks(width / 16, height / 16);
 			const dim3 threads(16, 16);
 			if (depth_bits == 16) {
-				check_consistency_kernel << < blocks, threads >> > (d_left_disp, d_right_disp, (uint16_t*)d_src_left, width, height);
+				check_consistency_kernel2<uint16_t> << < blocks, threads >> > (d_left_disp, d_right_disp, (uint16_t*)d_src_left, width, height);
+			}
+			else if (depth_bits == 8) {
+				check_consistency_kernel2<uint8_t> << < blocks, threads >> > (d_left_disp, d_right_disp, (uint8_t*)d_src_left, width, height);
 			}
 			
 			CudaKernelCheck();	
