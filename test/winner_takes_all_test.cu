8909140efad1ffce5aca4fa122d2f8eda38f0906
shingo.otsuka <shingo.otsuka@fixstars.com>
2018-10-05 19:05:48 +0900

    Fix winner takes all test for subpixel

--- test/winner_takes_all_test.cu
+++ test/winner_takes_all_test.cu
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <utility>
+#include <libsgm.h>
 #include "winner_takes_all.hpp"
 #include "generator.hpp"
 #include "test_utility.hpp"
@@ -13,7 +14,7 @@ static constexpr size_t NUM_PATHS = 8;
 thrust::host_vector<sgm::output_type> winner_takes_all_left(
 	const thrust::host_vector<sgm::cost_type>& src,
 	size_t width, size_t height, size_t disparity,
-	float uniqueness)
+	float uniqueness, bool subpixel)
 {
 	thrust::host_vector<sgm::output_type> result(width * height);
 	for(size_t i = 0; i < height; ++i){
@@ -30,6 +31,7 @@ thrust::host_vector<sgm::output_type> winner_takes_all_left(
 				}
 				v.emplace_back(cost_sum, static_cast<int>(k));
 			}
+			auto w = v;
 			sort(v.begin(), v.end());
 			if(v.size() < 2){
 				result[i * width + j] = 0;
@@ -38,10 +40,23 @@ thrust::host_vector<sgm::output_type> winner_takes_all_left(
 				const int cost1 = v[1].first;
 				const int disp0 = v[0].second;
 				const int disp1 = v[1].second;
-				result[i * width + j] = static_cast<sgm::output_type>(
-					(cost1 * uniqueness < cost0 && abs(disp0 - disp1) > 1)
-						? 0
-						: disp0);
+				sgm::output_type dst;
+				if (cost1 * uniqueness < cost0 && abs(disp0 - disp1) > 1) {
+					dst = 0;
+				} else {
+					dst = disp0;
+					if (subpixel) {
+						dst <<= sgm::StereoSGM::SUBPIXEL_SHIFT;
+						if (0 < disp0 && disp0 < disparity - 1) {
+							const int left = w[disp0 - 1].first;
+							const int right = w[disp0 + 1].first;
+							const int numer = left - right;
+							const int denom = left - 2 * cost0 + right;
+							dst += ((numer << sgm::StereoSGM::SUBPIXEL_SHIFT) + denom) / (2 * denom);
+						}
+					}
+				}
+				result[i * width + j] = dst;
 			}
 		}
 	}
@@ -88,25 +103,35 @@ thrust::host_vector<sgm::output_type> winner_takes_all_right(
 
 }
 
-
-TEST(WinnerTakesAllTest, RandomLeft){
+static void test_random_left(bool subpixel)
+{
 	static constexpr size_t width = 313, height = 237, disparity = 128;
 	static constexpr float uniqueness = 0.95f;
 	const auto input = generate_random_sequence<sgm::cost_type>(
-		width * height * disparity * NUM_PATHS);
+	width * height * disparity * NUM_PATHS);
 	const auto expect = winner_takes_all_left(
-		input, width, height, disparity, uniqueness);
+		input, width, height, disparity, uniqueness, subpixel);
 
 	sgm::WinnerTakesAll<disparity> wta;
 	const auto d_input = to_device_vector(input);
-	wta.enqueue(d_input.data().get(), width, height, uniqueness, 0);
+	wta.enqueue(d_input.data().get(), width, height, uniqueness, subpixel, 0);
 	cudaStreamSynchronize(0);
 
 	const thrust::device_vector<sgm::output_type> d_actual(
 		wta.get_left_output(), wta.get_left_output() + (width * height));
 	const auto actual = to_host_vector(d_actual);
+
 	EXPECT_EQ(actual, expect);
 	debug_compare(actual.data(), expect.data(), width, height, 1);
+}
+
+
+TEST(WinnerTakesAllTest, RandomLeftNormal){
+	test_random_left(false);
+}
+
+TEST(WinnerTakesAllTest, RandomLeftSubpixel){
+	test_random_left(true);
 }
 
 TEST(WinnerTakesAllTest, RandomRight){
@@ -119,7 +144,7 @@ TEST(WinnerTakesAllTest, RandomRight){
 
 	sgm::WinnerTakesAll<disparity> wta;
 	const auto d_input = to_device_vector(input);
-	wta.enqueue(d_input.data().get(), width, height, uniqueness, 0);
+	wta.enqueue(d_input.data().get(), width, height, uniqueness, false, 0);
 	cudaStreamSynchronize(0);
 
 	const thrust::device_vector<sgm::output_type> d_actual(
