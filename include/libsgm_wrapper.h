f843316b8ae76df3137936a0181d6b0c5f29da78
Shingo Otsuka <shingo.otsuka@fixstars.com>
2018-11-19 15:36:25 +0900

    Add description for LibSGMWrapper

--- include/libsgm_wrapper.h
+++ include/libsgm_wrapper.h
@@ -8,15 +8,39 @@
 #endif
 
 namespace sgm {
+	/**
+	 * @brief LibSGMWrapper class which is wrapper for sgm::StereoSGM.
+	 */
 	class LibSGMWrapper {
 	public:
 		static constexpr size_t DISPARITY_SIZE = 128;
 		static constexpr int OUTPUT_DEPTH_BITS = 16;
+		/**
+		 * @param param You can specify parameters.  See libsgm.h for more information.
+		 */
 		LibSGMWrapper(const sgm::StereoSGM::Parameters& param = {});
 		~LibSGMWrapper();
 
 #ifdef WITH_OPENCV
+		/**
+		 * Execute stereo semi global matching via wrapper class
+		 * @param I1        Input left image.  Image's type is must be CV_8U or CV_16U
+		 * @param I2        Input right image.  Image's size and type must be same with I1.
+		 * @param disparity Output image.  Its memory will be allocated automatically dependent on input image size.
+		 * @attention
+		 * type of output image `disparity` is CV_16U.
+		 * Note that dst element value would be multiplied StereoSGM::SUBPIXEL_SCALE if subpixel option was enabled.
+		 */
 		void execute(const cv::cuda::GpuMat& I1, const cv::cuda::GpuMat& I2, cv::cuda::GpuMat& disparity);
+		/**
+		 * Execute stereo semi global matching via wrapper class
+		 * @param I1        Input left image.  Image's type is must be CV_8U or CV_16U
+		 * @param I2        Input right image.  Image's size and type must be same with I1.
+		 * @param disparity Output image.  Its memory will be allocated automatically dependent on input image size.
+		 * @attention
+		 * type of output image `disparity` is CV_16U.
+		 * Note that dst element value would be multiplied StereoSGM::SUBPIXEL_SCALE if subpixel option was enabled.
+		 */
 		void execute(const cv::Mat& I1, const cv::Mat& I2, cv::Mat& disparity);
 #endif // WITH_OPENCV
 
