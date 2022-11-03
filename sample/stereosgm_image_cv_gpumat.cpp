4061b01d7ec0bf3501ad537974972283579a5595
akihiro.takagi <akihiro.takagi@fixstars.com>
2022-11-03 17:34:58 +0900

    Refactor sample image GpuMat

--- sample/stereosgm_image_cv_gpumat.cpp
+++ sample/stereosgm_image_cv_gpumat.cpp
@@ -14,42 +14,58 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include <stdlib.h>
-#include <iostream>
-
-#include <opencv2/core/core.hpp>
-#include <opencv2/core/cuda.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
 #include <libsgm.h>
 
-#define ASSERT_MSG(expr, msg) \
-	if (!(expr)) { \
-		std::cerr << msg << std::endl; \
-		std::exit(EXIT_FAILURE); \
-	} \
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "sample_common.h"
+
+static const std::string keys =
+"{@left_img   | <none> | path to input left image                                                            }"
+"{@right_img  | <none> | path to input right image                                                           }"
+"{disp_size   |     64 | maximum possible disparity value                                                    }"
+"{P1          |     10 | penalty on the disparity change by plus or minus 1 between nieghbor pixels          }"
+"{P2          |    120 | penalty on the disparity change by more than 1 between neighbor pixels              }"
+"{uniqueness  |   0.95 | margin in ratio by which the best cost function value should be at least second one }"
+"{num_paths   |      8 | number of scanlines used in cost aggregation                                        }"
+"{min_disp    |      0 | minimum disparity value                                                             }"
+"{LR_max_diff |      1 | maximum allowed difference between left and right disparity                         }"
+"{help h      |        | display this help and exit                                                          }";
 
 int main(int argc, char* argv[])
 {
-	ASSERT_MSG(argc >= 3, "usage: stereosgm left_img right_img [disp_size]");
+	cv::CommandLineParser parser(argc, argv, keys);
+	if (argc < 3 || parser.has("help")) {
+		parser.printMessage();
+		return 0;
+	}
 
-	cv::Mat  left = cv::imread(argv[1], -1);
-	cv::Mat right = cv::imread(argv[2], -1);
-	const int disp_size = argc > 3 ? std::atoi(argv[3]) : 128;
+	cv::Mat I1 = cv::imread(parser.get<cv::String>("@left_img"), cv::IMREAD_UNCHANGED);
+	cv::Mat I2 = cv::imread(parser.get<cv::String>("@right_img"), cv::IMREAD_UNCHANGED);
 
-	ASSERT_MSG(left.size() == right.size() && left.type() == right.type(), "input images must be same size and type.");
-	ASSERT_MSG(left.type() == CV_8U || left.type() == CV_16U, "input image format must be CV_8U or CV_16U.");
+	const int disp_size = parser.get<int>("disp_size");
+	const int P1 = parser.get<int>("P1");
+	const int P2 = parser.get<int>("P2");
+	const float uniqueness = parser.get<float>("uniqueness");
+	const int num_paths = parser.get<int>("num_paths");
+	const int min_disp = parser.get<int>("min_disp");
+	const int LR_max_diff = parser.get<int>("LR_max_diff");
+
+	ASSERT_MSG(!I1.empty() && !I2.empty(), "imread failed.");
+	ASSERT_MSG(I1.size() == I2.size() && I1.type() == I2.type(), "input images must be same size and type.");
+	ASSERT_MSG(I1.type() == CV_8U || I1.type() == CV_16U, "input image format must be CV_8U or CV_16U.");
 	ASSERT_MSG(disp_size == 64 || disp_size == 128 || disp_size == 256, "disparity size must be 64, 128 or 256.");
+	ASSERT_MSG(num_paths == 4 || num_paths == 8, "number of scanlines must be 4 or 8.");
 
-	sgm::LibSGMWrapper sgm(disp_size);
+	const sgm::PathType path_type = num_paths == 8 ? sgm::PathType::SCAN_8PATH : sgm::PathType::SCAN_4PATH;
+	sgm::LibSGMWrapper sgm(disp_size, P1, P2, uniqueness, false, path_type, min_disp, LR_max_diff);
 	cv::Mat disparity;
 
 	try {
-		cv::cuda::GpuMat d_left, d_right, d_disparity;
-		d_left.upload(left);
-		d_right.upload(right);
-		sgm.execute(d_left, d_right, d_disparity);
+		cv::cuda::GpuMat d_I1(I1), d_I2(I2), d_disparity;
+		sgm.execute(d_I1, d_I2, d_disparity);
 		d_disparity.download(disparity);
 	}
 	catch (const cv::Exception& e) {
@@ -57,18 +73,20 @@ int main(int argc, char* argv[])
 		return e.code == cv::Error::GpuNotSupported ? 1 : -1;
 	}
 
-	// show image
-	cv::Mat mask = disparity == sgm.getInvalidDisparity();
-	cv::Mat disparity_color;
-	disparity.convertTo(disparity, CV_8U, 255. / disp_size);
-	cv::applyColorMap(disparity, disparity_color, cv::COLORMAP_JET);
-	disparity.setTo(0, mask);
-	disparity_color.setTo(cv::Scalar(0, 0, 0), mask);
-	if (left.type() != CV_8U)
-		cv::normalize(left, left, 0, 255, cv::NORM_MINMAX, CV_8U);
+	// create mask for invalid disp
+	const cv::Mat mask = disparity == sgm.getInvalidDisparity();
 
-	std::vector<cv::Mat> images = { disparity, disparity_color, left };
-	std::vector<std::string> titles = { "disparity", "disparity color", "input" };
+	// show image
+	cv::Mat disparity_8u, disparity_color;
+	disparity.convertTo(disparity_8u, CV_8U, 255. / disp_size);
+	cv::applyColorMap(disparity_8u, disparity_color, cv::COLORMAP_TURBO);
+	disparity_8u.setTo(0, mask);
+	disparity_color.setTo(cv::Scalar::all(0), mask);
+	if (I1.type() != CV_8U)
+		cv::normalize(I1, I1, 0, 255, cv::NORM_MINMAX, CV_8U);
+
+	const std::vector<cv::Mat> images = { disparity_8u, disparity_color, I1 };
+	const std::vector<std::string> titles = { "disparity", "disparity color", "input" };
 
 	std::cout << "Hot keys:" << std::endl;
 	std::cout << "\tESC - quit the program" << std::endl;
