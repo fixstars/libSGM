ef5ccc58b45e1ff45afc4f6522f8138b16e87ffb
akihiro.takagi <akihiro.takagi@fixstars.com>
2022-11-04 15:32:15 +0900

    Update ZED sample

--- sample/stereosgm_zed.cpp
+++ sample/stereosgm_zed.cpp
@@ -14,97 +14,101 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include <stdlib.h>
-#include <cstdint>
 #include <iostream>
-#include <iomanip>
-#include <string>
 #include <chrono>
 
-#include <cuda_runtime.h>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 #include <sl/Camera.hpp>
 
 #include <libsgm.h>
 
-template <class... Args>
-static std::string format_string(const char* fmt, Args... args)
+#include "sample_common.h"
+
+static const std::string keys =
+"{disp_size           |    128 | maximum possible disparity value                  }"
+"{camera_resolution   |      3 | camera resolution (0:HD2K 1:HD1080 2:HD720 3:VGA) }"
+"{help h              |        | display this help and exit                        }";
+
+int main(int argc, char* argv[])
 {
-	const int BUF_SIZE = 1024;
-	char buf[BUF_SIZE];
-	std::snprintf(buf, BUF_SIZE, fmt, args...);
-	return std::string(buf);
-}
+	cv::CommandLineParser parser(argc, argv, keys);
+	if (parser.has("help")) {
+		parser.printMessage();
+		return 0;
+	}
 
-struct device_buffer {
-	device_buffer() : data(nullptr) {}
-	device_buffer(size_t count) { allocate(count); }
-	void allocate(size_t count) { cudaMalloc(&data, count); }
-	~device_buffer() { cudaFree(data); }
-	void* data;
-};
+	const int disp_size = parser.get<int>("disp_size");
+	const sl::RESOLUTION camera_resolution = parser.get<sl::RESOLUTION>("camera_resolution");
 
-int main(int argc, char* argv[]) {	
-	
-	const int disp_size = 128;
-	
 	sl::Camera zed;
 	sl::InitParameters initParameters;
-	initParameters.camera_resolution = sl::RESOLUTION::VGA;
-	sl::ERROR_CODE err = zed.open(initParameters);
+	initParameters.camera_resolution = camera_resolution;
+	const sl::ERROR_CODE err = zed.open(initParameters);
 	if (err != sl::ERROR_CODE::SUCCESS) {
-		std::cout << toString(err) << std::endl;
-		zed.close();
-		return 1;
+		std::cerr << sl::toString(err) << std::endl;
+		std::exit(EXIT_FAILURE);
 	}
-	const int width = static_cast<int>(zed.getCameraInformation().camera_configuration.resolution.width);
-	const int height = static_cast<int>(zed.getCameraInformation().camera_configuration.resolution.height);
 
-	sl::Mat d_zed_image_l(zed.getCameraInformation().camera_configuration.resolution, sl::MAT_TYPE::U8_C1, sl::MEM::GPU);
-	sl::Mat d_zed_image_r(zed.getCameraInformation().camera_configuration.resolution, sl::MAT_TYPE::U8_C1, sl::MEM::GPU);
+	const auto& resolution = zed.getCameraInformation().camera_configuration.resolution;
+	sl::Mat d_zed_image_L(resolution, sl::MAT_TYPE::U8_C1, sl::MEM::GPU);
+	sl::Mat d_zed_image_R(resolution, sl::MAT_TYPE::U8_C1, sl::MEM::GPU);
+	CV_Assert(d_zed_image_L.getStep(sl::MEM::GPU) == d_zed_image_R.getStep(sl::MEM::GPU));
 
-	const int input_depth = 8;
-	const int output_depth = 8;
-	const int output_bytes = output_depth * width * height / 8;
+	const int width = resolution.width;
+	const int height = resolution.height;
+	const int src_pitch = static_cast<int>(d_zed_image_L.getStep(sl::MEM::GPU));
+	const int dst_pitch = width;
 
-	CV_Assert(d_zed_image_l.getStep(sl::MEM::GPU) == d_zed_image_r.getStep(sl::MEM::GPU));
-	sgm::StereoSGM sgm(width, height, disp_size, input_depth, output_depth, static_cast<int>(d_zed_image_l.getStep(sl::MEM::GPU)), width, sgm::EXECUTE_INOUT_CUDA2CUDA);
+	const int src_depth = 8;
+	const int dst_depth = disp_size < 256 ? 8 : 16;
+	const int src_bytes = src_depth * width * height / 8;
+	const int dst_bytes = dst_depth * width * height / 8;
 
-	cv::Mat disparity(height, width, CV_8U);
-	cv::Mat disparity_8u, disparity_color;
+	sgm::StereoSGM sgm(width, height, disp_size, src_depth, dst_depth, src_pitch, dst_pitch, sgm::EXECUTE_INOUT_CUDA2CUDA);
 
-	device_buffer d_disparity(output_bytes);
+	device_buffer d_disparity(dst_bytes);
+	cv::Mat disparity(height, width, dst_depth == 8 ? CV_8S : CV_16S), disparity_color;
+
+	const int invalid_disp = sgm.get_invalid_disparity();
+
+	std::cout << "max disparity    : " << disp_size << std::endl;
+	std::cout << "camera resolution: " << sl::toString(initParameters.camera_resolution) << " " << cv::Size(width, height) << std::endl;
+
 	while (1) {
+
 		if (zed.grab() == sl::ERROR_CODE::SUCCESS) {
-			zed.retrieveImage(d_zed_image_l, sl::VIEW::LEFT_GRAY, sl::MEM::GPU);
-			zed.retrieveImage(d_zed_image_r, sl::VIEW::RIGHT_GRAY, sl::MEM::GPU);
-		} else continue;
+			zed.retrieveImage(d_zed_image_L, sl::VIEW::LEFT_GRAY, sl::MEM::GPU);
+			zed.retrieveImage(d_zed_image_R, sl::VIEW::RIGHT_GRAY, sl::MEM::GPU);
+		}
+		else {
+			continue;
+		}
 
 		const auto t1 = std::chrono::system_clock::now();
 
-		sgm.execute(d_zed_image_l.getPtr<uchar>(sl::MEM::GPU), d_zed_image_r.getPtr<uchar>(sl::MEM::GPU), d_disparity.data);
+		sgm.execute(d_zed_image_L.getPtr<uchar>(sl::MEM::GPU), d_zed_image_R.getPtr<uchar>(sl::MEM::GPU), d_disparity.data);
 		cudaDeviceSynchronize();
 
 		const auto t2 = std::chrono::system_clock::now();
 		const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 		const double fps = 1e6 / duration;
 
-		cudaMemcpy(disparity.data, d_disparity.data, output_bytes, cudaMemcpyDeviceToHost);
+		d_disparity.download(disparity.data);
 
-		disparity.convertTo(disparity_8u, CV_8U, 255. / disp_size);
-		cv::applyColorMap(disparity_8u, disparity_color, cv::COLORMAP_JET);
-		disparity_color.setTo(cv::Scalar(0, 0, 0), disparity == static_cast<uint8_t>(sgm.get_invalid_disparity()));
-		cv::putText(disparity_color, format_string("sgm execution time: %4.1f[msec] %4.1f[FPS]", 1e-3 * duration, fps),
-			cv::Point(50, 50), 2, 0.75, cv::Scalar(255, 255, 255));
+		// draw results
+		colorize_disparity(disparity, disparity_color, disp_size, disparity == invalid_disp);
+		cv::putText(disparity_color, cv::format("sgm execution time: %4.1f[msec] %4.1f[FPS]",
+			1e-3 * duration, fps), cv::Point(50, 50), 2, 0.75, cv::Scalar(255, 255, 255));
 
 		cv::imshow("disparity", disparity_color);
+
 		const char c = cv::waitKey(1);
 		if (c == 27) // ESC
 			break;
 	}
-	zed.close();
+
 	return 0;
 }
